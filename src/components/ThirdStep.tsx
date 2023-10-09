73946592b381a7d1152f89385f71b4e415ad626b
kwangminNam <johnpower@naver.com>
2023-10-09 22:59:56 +0900

    add persist zustand midlleware

--- src/components/ThirdStep.tsx
+++ src/components/ThirdStep.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useDataStore } from "../stores/fetchDataStore";
 
 export default function ThirdStep() {
+  const { data, status, getData, changeStatus, getSeocndData } = useDataStore();
+
+  console.log(data);
   return (
     <div>
-      
+      <button onClick={() => changeStatus("BYE!")}>CHANGE STATUS</button>
+      <h1>{status}</h1>
+      <button
+        onClick={() => {
+          getData(1);
+        }}
+      >
+        FETCH DATA
+      </button>
+      <button onClick={()=>getData(2)}>SECOND DATA</button>
+
+      <h1>
+        {data.id}
+        {data.title}
+      </h1>
     </div>
-  )
+  );
 }
--- src/stores/fetchDataStore.ts
+++ src/stores/fetchDataStore.ts
@@ -1,21 +1,39 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 
 interface DataTypes {
   data: any;
-  test: string;
-  getData: (id: number) => void;
+  status: string;
+  getData: (id:number) => void;
+  getSeocndData: (id:number) => void;
+  changeStatus:(status:string) => void;
+  
 }
 
-export const useDataStore = create<DataTypes>()((set) => ({
+export const useDataStore = create<DataTypes>()(persist((set) => ({
   data: undefined,
-  test: 'HI',
-  getData: async (id: number) => {
-    const data = fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-    const res = (await data).json();
+  status: 'HI',
+  changeStatus: (change) => set((state)=>({
+    ...state,
+    status:change
+  })),
+  getData: async (id:number) => {
+    const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    const res = await data.json();
     set((state) => ({
       ...state,
       data: res
     }))
-  }
+  },
+  getSeocndData: async (id:number) => {
+    const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+    const res = await data.json();
+    set((state) => ({
+      ...state,
+      data: res
+    }))
+  },
+}),{
+  name:'fetch-data'
 }))
